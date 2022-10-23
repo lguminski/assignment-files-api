cfb0c19ee35e20816ca1407bfae1d29e66d4e897
Lukasz Guminski <lguminski@gmail.com>
2022-10-23 20:35:46 +0200

    implementing s3 list

--- api-server/src/server.rs
+++ api-server/src/server.rs
@@ -6,15 +6,18 @@ use async_trait::async_trait;
 use futures::{future, Stream, StreamExt, TryFutureExt, TryStreamExt};
 use hyper::server::conn::Http;
 use hyper::service::Service;
-use log::info;
+use log::{error, info};
+use s3::bucket::Bucket;
+use s3::creds::Credentials;
+use std::env;
 use std::future::Future;
 use std::marker::PhantomData;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
-use swagger::{Has, XSpanIdString};
 use swagger::auth::MakeAllowAllAuthenticator;
 use swagger::EmptyContext;
+use swagger::{Has, XSpanIdString};
 use tokio::net::TcpListener;
 
 #[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
@@ -34,9 +37,7 @@ pub async fn create(addr: &str, https: bool) {
 
     #[allow(unused_mut)]
     let mut service =
-        openapi_client::server::context::MakeAddContext::<_, EmptyContext>::new(
-            service
-        );
+        openapi_client::server::context::MakeAddContext::<_, EmptyContext>::new(service);
 
     if https {
         #[cfg(any(target_os = "macos", target_os = "windows", target_os = "ios"))]
@@ -46,12 +47,16 @@ pub async fn create(addr: &str, https: bool) {
 
         #[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
         {
-            let mut ssl = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls()).expect("Failed to create SSL Acceptor");
+            let mut ssl = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls())
+                .expect("Failed to create SSL Acceptor");
 
             // Server authentication
-            ssl.set_private_key_file("examples/server-key.pem", SslFiletype::PEM).expect("Failed to set private key");
-            ssl.set_certificate_chain_file("examples/server-chain.pem").expect("Failed to set certificate chain");
-            ssl.check_private_key().expect("Failed to check private key");
+            ssl.set_private_key_file("examples/server-key.pem", SslFiletype::PEM)
+                .expect("Failed to set private key");
+            ssl.set_certificate_chain_file("examples/server-chain.pem")
+                .expect("Failed to set certificate chain");
+            ssl.check_private_key()
+                .expect("Failed to check private key");
 
             let tls_acceptor = ssl.build();
             let tcp_listener = TcpListener::bind(&addr).await.unwrap();
@@ -76,7 +81,10 @@ pub async fn create(addr: &str, https: bool) {
         }
     } else {
         // Using HTTP
-        hyper::server::Server::bind(&addr).serve(service).await.unwrap()
+        hyper::server::Server::bind(&addr)
+            .serve(service)
+            .await
+            .unwrap()
     }
 }
 
@@ -87,29 +95,72 @@ pub struct Server<C> {
 
 impl<C> Server<C> {
     pub fn new() -> Self {
-        Server{marker: PhantomData}
+        Server {
+            marker: PhantomData,
+        }
     }
 }
 
-
-use openapi_client::{
-    Api,
-    FilesGetResponse,
-};
 use openapi_client::server::MakeService;
+use openapi_client::{Api, FilesGetResponse};
 use std::error::Error;
 use swagger::ApiError;
 
 #[async_trait]
-impl<C> Api<C> for Server<C> where C: Has<XSpanIdString> + Send + Sync
+impl<C> Api<C> for Server<C>
+where
+    C: Has<XSpanIdString> + Send + Sync,
 {
-    async fn files_get(
-        &self,
-        context: &C) -> Result<FilesGetResponse, ApiError>
-    {
-        let context = context.clone();
-        info!("files_get() - X-Span-ID: {:?}", context.get().0.clone());
-        Err(ApiError("Generic failure".into()))
-    }
+    async fn files_get(&self, context: &C) -> Result<FilesGetResponse, ApiError> {
+        let _context = context.clone();
 
+        let bucket_name = match env::var("AWS_S3_BUCKET") {
+            Ok(bucket_name) => bucket_name,
+            Err(err) => return Err(ApiError(format!("Invalid bucket: {}", err))),
+        };
+
+        let region = match env::var("AWS_REGION") {
+            Ok(region) => region,
+            Err(err) => {
+                return Err(ApiError(format!(
+                    "Missing AWS_REGION env variable: {}",
+                    err
+                )))
+            }
+        };
+
+        let region = match region.parse() {
+            Ok(region) => region,
+            Err(err) => return Err(ApiError(format!("Invalid region: {}", err))),
+        };
+
+        let credentials = match Credentials::from_env() {
+            Ok(credentials) => credentials,
+            Err(err) => return Err(ApiError(format!("Invalid credentials: {}", err))),
+        };
+
+        let bucket = match Bucket::new(&bucket_name, region, credentials) {
+            Ok(bucket) => bucket,
+            Err(err) => return Err(ApiError(format!("Invalid bucket: {}", err))),
+        };
+
+        let results = match bucket.list(String::default(), Some("/".to_string())).await {
+            Ok(results) => results,
+            Err(err) => {
+                return Err(ApiError(format!("Failed to list bucket: {}", err)));
+            }
+        };
+
+        let files = results
+            .first()
+            .unwrap()
+            .contents
+            .iter()
+            .map(|x| models::FilesGet200ResponseInner {
+                file_name: Some(x.key.to_owned()),
+            })
+            .collect();
+
+        Ok(FilesGetResponse::AListOfItemsFromTheObjectBucket(files))
+    }
 }
